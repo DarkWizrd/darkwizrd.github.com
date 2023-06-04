9053e48b051584f1645ed419daafb50233960efc
Omar Méndez <mastercool507@hotmail.com>
2023-06-04 11:02:13 -0500

    Carga de datos del domingo 04/06/23

--- DSIX/Laboratorios/lab2_OmarMendez/Problema5/main.js
+++ DSIX/Laboratorios/lab2_OmarMendez/Problema5/main.js
@@ -1,5 +1,6 @@
 /*Genere un script externo que calcule el valor factorial de un número escrito por el usuario y le muestre su resultado por pantalla y consola. El factorial de un número entero n es una operación matemática que consiste en multiplicar todos los factores n x (n-1) x (n-2) x... 1. Así, el factorial de 5 (escrito como 5!) es igual a: 5*4*3*2*1*/
-let num = 0, factorial = 1;
+let num = 0,
+  factorial = 1;
 num = prompt("Digite un numero para calcular su factorial");
 if (num != null) {
   num = parseFloat(num);
---
+++ DSIX/Laboratorios/lab3_OmarMendez/main.js
@@ -0,0 +1,58 @@
+//problema 1
+function bisiesto() {
+  let anno, mensaje;
+  anno = parseInt(prompt("Digite un año"));
+  if ((anno % 4 === 0 && anno % 100 !== 0) || anno % 400 === 0) {
+    mensaje = anno + " es un año bisiesto.";
+  } else {
+    mensaje = anno + " es un año normal.";
+  }
+  if (anno < 1582) {
+    mensaje += "\nEste año no corresponde a la era gregoriana";
+  }
+  alert(mensaje);
+}
+//problema 2
+function imc(peso, altura) {
+  let imc, mensaje;
+  imc = (peso * 0.4536) / (altura * altura);
+  imc = imc.toFixed(2);
+  if (imc > 25) {
+    mensaje =
+      "Cuidado, estás en sobrepeso.\nTu indice de masa corporal es: " + imc;
+  } else if (imc < 18.5) {
+    mensaje =
+      "Cuidado, estas por debajo de tu peso ideal.\nTu indice de masa corporal es: " +
+      imc;
+  } else {
+    mensaje =
+      "Felicidades, estas en tu rango de peso normal\nTu indice de masa corporal es: " +
+      imc;
+  }
+  mensaje += "\nRecuera alimentarte bien y hacer ejercicio regularmente.";
+  return mensaje;
+}
+//Problema 3
+function pares(num) {
+  if (num % 2 === 0) {
+    return "El numero es par.";
+  } else {
+    return "El numero es impar.";
+  }
+}
+//problema 4
+function piramide(bloques) {
+  let altura = 0, base = 0, material;
+  material = bloques;
+  while(material >= (base + 1)) {
+    altura += 1;
+    base += 1;
+    material -= base;
+  }
+  if (material === 0) {
+    return ("La altura de la pirámide es de " + altura + " bloques de altura\nNo sobran bloques");
+  } else {
+    return("La altura de la pirámide es de " + altura + " bloques de altura\nCon "+material+" bloques sobrantes");
+  }
+
+}
